06258bf2757e8866812560fb422c8049f037902c
Frusadev <frusadev@gmail.com>
2025-10-06 11:57:21 +0000

    feat(partners): add new partners - Rekap - Cyber Defense Africa - Ijeaf - ISOC Togo - Night coding - Cursor TG

--- src/lib/partners.ts
+++ src/lib/partners.ts
@@ -6,19 +6,9 @@ export type Partner = {
 
 export const PARTNERS: Partner[] = [
   {
-    name: "Python Togo",
-    imageFile: "/partners/pytg.png",
-    url: "https://pytogo.org",
-  },
-  {
     name: "Institut Françqais du Togo",
     imageFile: "/partners/ift.jpg",
     url: "https://institutfrancais-togo.com",
-  },
-  {
-    name: "Africa Networking Premium",
-    imageFile: "/partners/anp.jpg",
-    url: "https://www.facebook.com/AfricaNetworkingPremium",
   },
   {
     name: "JetBrains",
@@ -26,8 +16,53 @@ export const PARTNERS: Partner[] = [
     url: "https://www.jetbrains.com",
   },
   {
+    name: "Cyber Defense Africa",
+    imageFile: "/partners/cda.png",
+    url: "https://www.cda.tg",
+  },
+  {
     name: "OrgaAfrica",
     imageFile: "/partners/orga.png",
     url: "https://www.orga-africa.com",
+  },
+  {
+    name: "Python Togo",
+    imageFile: "/partners/pytg.png",
+    url: "https://pytogo.org",
+  },
+  {
+    name: "Africa Networking Premium",
+    imageFile: "/partners/anp.jpg",
+    url: "https://www.facebook.com/AfricaNetworkingPremium",
+  },
+  {
+    name: "Ethix",
+    imageFile: "/partners/ethix.png",
+    url: "#",
+  },
+  {
+    name: "Ijeaf",
+    imageFile: "/partners/ijeaf.png",
+    url: "https://ijeaf.africa",
+  },
+  {
+    name: "Night Coding",
+    imageFile: "/partners/nc.png",
+    url: "https://www.linkedin.com/company/night-coding-2023",
+  },
+  {
+    name: "Internet Society Chapitre Togo",
+    imageFile: "/partners/isoc.png",
+    url: "https://internetsociety.tg/",
+  },
+  {
+    name: "Cursor Togo",
+    imageFile: "/partners/curtg.png",
+    url: "https://www.linkedin.com/company/cursor-togo-community",
+  },
+  {
+    name: "Rekap",
+    imageFile: "/partners/rekap.png",
+    url: "https://rekap.media",
   },
 ];
